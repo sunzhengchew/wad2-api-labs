cf8cf5c394312980886c12814e0558023d8c1f97
sunzhengchew <szcheww@icloud.com>
2025-12-03 04:21:56 +0000

    Express Lab - exercise done

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -21,13 +21,17 @@ router.get('/:id', (req, res) => {
 //Add a task
 router.post('/', (req, res) => {
     const { title, description, deadline, priority, done } = req.body;
+    const timestamp = new Date().toISOString();
+
     const newTask = {
         id: uuidv4(),
         title,
         description,
         deadline,
         priority,
-        done
+        done,
+        create_at: timestamp,
+        update_at: timestamp
     };
     tasksData.tasks.push(newTask);
     res.status(201).json(newTask);
@@ -38,10 +42,11 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {
     const { id } = req.params;
     const taskIndex = tasksData.tasks.findIndex(task => task.id === id); 
+    const timestamp = new Date().toISOString();
     if (taskIndex === -1) {
         return res.status(404).json({ status: 404, message: 'Task not found' });
     }
-    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id:id };
+    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id:id,updated_at: timestamp};
     tasksData.tasks[taskIndex] = updatedTask;
     res.json(updatedTask);
 });
