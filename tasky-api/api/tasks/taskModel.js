f1960193dcba89e025dabf0edb76e7ea7e031bdc
sunzhengchew <szcheww@icloud.com>
2025-12-04 01:23:07 +0000

    Added userId to tasks

--- tasky-api/api/tasks/taskModel.js
+++ tasky-api/api/tasks/taskModel.js
@@ -4,12 +4,16 @@ const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
   title: { type: String, required: true },
-  description:  String ,
+  description: String,
   deadline: Date,
   done: Boolean,
   priority: { type: String, enum: ["Low", "Medium", "High"], required: true },
   created_at: Date,
-  updated_at: Date
+  updated_at: Date,
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
 });
 
 const dateValidator = (date) => {
