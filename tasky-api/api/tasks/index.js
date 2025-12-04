3475dd2ded07ebf086bf4f52bf9b0f5627fb3ea4
sunzhengchew <szcheww@icloud.com>
2025-12-04 02:40:11 +0000

    Tasks related to User - lab completed

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -4,9 +4,10 @@ import asyncHandler from 'express-async-handler';
 
 const router = express.Router(); // eslint-disable-line
 
-// Get all tasks
+// Get a user's tasks
 router.get('/', async (req, res) => {
-    const tasks = await Task.find().populate('userId', 'username');
+    console.log(req.user);
+    const tasks = await Task.find({ userId: `${req.user._id}`});
     res.status(200).json(tasks);
 });
 
@@ -18,9 +19,12 @@ router.get('/user/:uid', async (req, res) => {
 
 // create a task
 router.post('/', asyncHandler(async (req, res) => {
-    const task = await Task(req.body).save();
+    const newTask = req.body;
+    newTask.userId = req.user._id;
+    const task = await Task(newTask).save();
     res.status(201).json(task);
 }));
+
 
 // Update Task
 router.put('/:id', async (req, res) => {
